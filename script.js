b4e7d7b65f15b3c48e1db68d310a02cffcc75736
Farid-Ziade <ziadefarid0@gmail.com>
2026-06-27 18:08:26 +0300

    Refactor gameBoard logic and enhance winning condition checks

--- script.js
+++ script.js
@@ -1,5 +1,5 @@
 "use strict";
-const gameBoard = (() => {
+let gameBoard = (() => {
   let board = [
     ["", "", ""],
     ["", "", ""],
@@ -12,19 +12,92 @@ const gameBoard = (() => {
       ["", "", ""],
     ];
   };
-  const placement = (row, column, marker) => {
-    if (board[row][column] !== "") return;
-    return (board[row][column] = marker);
+  let placement = (row, column, marker) => {
+    if (board[row][column] !== "") {
+      console.log("Please choose another cell this one is taken");
+      return true;
+    }
+    console.log("ok");
+    board[row][column] = marker;
+    console.table(board);
   };
-  return { reset, placement };
+  let winningCondition = (row, column, marker) => {
+    let win = board[row][0];
+    let gameOver;
+    if (
+      win !== "" &&
+      win == board[row][1] &&
+      board[row][1] == marker &&
+      win == board[row][2] &&
+      board[row][2] == marker
+    ) {
+      console.log(`${marker} win by row`);
+      return true;
+    }
+
+    win = board[0][column];
+
+    if (
+      win !== "" &&
+      win === board[1][column] &&
+      board[1][column] == marker &&
+      win === board[2][column] &&
+      board[2][column] == marker
+    ) {
+      console.log(`${marker} win by column`);
+      return true;
+    }
+
+    win = board[0][2];
+    if (
+      win !== "" &&
+      win === board[1][1] &&
+      board[1][1] == marker &&
+      win === board[2][0] &&
+      board[2][0] == marker
+    ) {
+      console.log(`${marker} won by the other diagonal`);
+      return true;
+    }
+
+    win = board[2][2];
+    if (
+      win !== "" &&
+      win === board[1][1] &&
+      board[1][1] == marker &&
+      win === board[0][0] &&
+      board[0][0] == marker
+    ) {
+      console.log(`${marker} won by the diagonal`);
+      return true;
+    }
+  };
+  return { reset, placement, winningCondition };
 })();
 
 function createPlayer(name, marker) {
   return { name, marker };
 }
 
-function gameControler(gameBoard) {
+function gameControler() {
   const player1 = createPlayer("Player 1", "X");
   const player2 = createPlayer("Player 2", "O");
+  let gameOver = false;
+  while (gameOver) {
+    let rows = Number(prompt("Please enter row "));
+    let columns = Number(prompt("Please enter column "));
+    let last = player1.marker;
+    gameBoard.placement(rows, columns, last);
+    while (gameBoard.placement(rows, columns, last)) {
+      rows = Number(prompt("Please enter row "));
+      columns = Number(prompt("Please enter column "));
+    }
+    gameBoard.winningCondition(rows, columns, last);
+    while (gameBoard.winningCondition(rows, columns, last)) {
+      gameOver = false;
+    }
+    last == "X" ? (last = player2.marker) : (last = player1.marker);
+  }
 }
-console.log(gameControler(gameBoard));
+gameControler();
+// gameBoard.placement(0, 2, "X");
