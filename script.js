c4b900a45117650d20e1bb71000f7353b5b518b3
Farid-Ziade <61830209@students.liu.edu.lb>
2026-07-01 01:52:59 +0300

    Update player score display and enhance winning condition logic

--- script.js
+++ script.js
@@ -5,7 +5,10 @@ let place;
 let counter = 0;
 let gameOn;
 let h2 = document.querySelector(".h2");
-
+let XScore = document.querySelector(".Xscore");
+let scoreX = 0;
+let OScore = document.querySelector(".Oscore");
+let scoreO = 0;
 let gameBoard = (() => {
   let board = [
     ["", "", ""],
@@ -28,47 +31,60 @@ let gameBoard = (() => {
     let win = board[row][0];
     if (
       board[row][0] == marker &&
-      win == board[row][1] &&
       board[row][1] == marker &&
-      win == board[row][2] &&
       board[row][2] == marker
     ) {
       h2.innerHTML = mark + " won by row";
       message.appendChild(h2);
-      button.forEach((btn) => {
-        if (btn.hasAttribute("disabled")) {
-          if (row == 0 && btn.innerHTML === mark) {
-            btn.setAttribute("style", "background-color:gold");
-          }
-          if (row == 1 && btn.innerHTML === mark) {
-            btn.setAttribute("style", "background-color:gold");
-          }
-          if (row == 2 && btn.innerHTML === mark) {
-            btn.setAttribute("style", "background-color:gold");
-          }
+      button.forEach((btn, index) => {
+        console.log(index, row);
+        if (
+          row == 0 &&
+          btn.innerHTML === mark &&
+          (index == 0 || index == 1 || index == 2)
+        ) {
+          btn.setAttribute("style", "background-color:gold");
+        } else if (
+          row == 1 &&
+          btn.innerHTML === mark &&
+          (index == 3 || index == 4 || index == 5)
+        ) {
+          btn.setAttribute("style", "background-color:gold");
+        } else if (
+          row == 2 &&
+          btn.innerHTML === mark &&
+          (index == 6 || index == 7 || index == 8)
+        ) {
+          btn.setAttribute("style", "background-color:gold");
         }
       });
       return true;
-    }
-
-    win = board[0][column];
-
-    if (
-      win !== "" &&
-      win === board[1][column] &&
+    } else if (
+      board[0][column] == marker &&
       board[1][column] == marker &&
-      win === board[2][column] &&
       board[2][column] == marker
     ) {
-      button.forEach((btn) => {
+      button.forEach((btn, index) => {
         if (btn.hasAttribute("disabled")) {
-          if (column == 0 && btn.innerHTML === mark) {
+          if (
+            column == 0 &&
+            btn.innerHTML === mark &&
+            (index == 0 || index == 3 || index == 6)
+          ) {
             btn.setAttribute("style", "background-color:gold");
           }
-          if (column == 1 && btn.innerHTML === mark) {
+          if (
+            column == 1 &&
+            btn.innerHTML === mark &&
+            (index == 1 || index == 4 || index == 7)
+          ) {
             btn.setAttribute("style", "background-color:gold");
           }
-          if (column == 2 && btn.innerHTML === mark) {
+          if (
+            column == 2 &&
+            btn.innerHTML === mark &&
+            (index == 2 || index == 5 || index == 8)
+          ) {
             btn.setAttribute("style", "background-color:gold");
           }
         }
@@ -87,15 +103,30 @@ let gameBoard = (() => {
       win === board[2][0] &&
       board[2][0] == marker
     ) {
-      button.forEach((btn) => {
+      button.forEach((btn, index) => {
         if (btn.hasAttribute("Disabled")) {
-          if (row == 0 && column == 2 && btn.innerHTML === mark) {
+          if (
+            row == 0 &&
+            column == 2 &&
+            btn.innerHTML === mark &&
+            (index == 2 || index == 4 || index == 6)
+          ) {
             btn.setAttribute("style", "background-color:gold");
           }
-          if (row == 1 && column == 1 && btn.innerHTML === mark) {
+          if (
+            row == 1 &&
+            column == 1 &&
+            btn.innerHTML === mark &&
+            (index == 2 || index == 4 || index == 6)
+          ) {
             btn.setAttribute("style", "background-color:gold");
           }
-          if (row == 2 && column == 0 && btn.innerHTML === mark) {
+          if (
+            row == 2 &&
+            column == 0 &&
+            btn.innerHTML === mark &&
+            (index == 2 || index == 4 || index == 6)
+          ) {
             btn.setAttribute("style", "background-color:gold");
           }
         }
@@ -114,15 +145,30 @@ let gameBoard = (() => {
       win === board[0][0] &&
       board[0][0] == marker
     ) {
-      button.forEach((btn) => {
+      button.forEach((btn, index) => {
         if (btn.hasAttribute("Disabled")) {
-          if (row == 2 && column == 2 && btn.innerHTML === mark) {
+          if (
+            row == 2 &&
+            column == 2 &&
+            btn.innerHTML === mark &&
+            (index == 0 || index == 4 || index == 8)
+          ) {
             btn.setAttribute("style", "background-color:gold");
           }
-          if (row == 1 && column == 1 && btn.innerHTML === mark) {
+          if (
+            row == 1 &&
+            column == 1 &&
+            btn.innerHTML === mark &&
+            (index == 0 || index == 4 || index == 8)
+          ) {
             btn.setAttribute("style", "background-color:gold");
           }
-          if (row == 0 && column == 0 && btn.innerHTML === mark) {
+          if (
+            row == 0 &&
+            column == 0 &&
+            btn.innerHTML === mark &&
+            (index == 0 || index == 4 || index == 8)
+          ) {
             btn.setAttribute("style", "background-color:gold");
           }
         }
@@ -194,14 +240,16 @@ function gameControler() {
     if (gameBoard.placement(rows, columns, mark) == true) {
       counter++;
       if (gameBoard.winningCondition(rows, columns, mark) == true) {
+        mark == "X"
+          ? (XScore.innerHTML = ++scoreX)
+          : (OScore.textContent = ++scoreO);
+
         button.forEach((btn) => {
           btn.disabled = true;
         });
-        gameOn = false;
       } else if (counter >= 9) {
         h2.innerHTML = "Nobody won it's a tie ";
         message.appendChild(h2);
-        gameOn = false;
         button.forEach((btn) => {
           btn.disabled = true;
         });
@@ -225,5 +273,31 @@ button.forEach((btn, index) => {
   });
 });
 let newgame = document.querySelector(".new-game");
-newgame.addEventListener("click", () => {});
+newgame.addEventListener("click", () => {
+  button.forEach((btn) => {
+    btn.removeAttribute("disabled");
+    btn.removeAttribute("style");
+    btn.innerHTML = "";
+    gameBoard.reset();
+    counter = 0;
+    gameOn = true;
+    h2.innerHTML = "";
+  });
+});
 let message = document.querySelector(".message");
+let resetButton = document.querySelector(".reset");
+resetButton.addEventListener("click", () => {
+  button.forEach((btn) => {
+    btn.removeAttribute("disabled");
+    btn.removeAttribute("style");
+    btn.innerHTML = "";
+    gameBoard.reset();
+    counter = 0;
+    gameOn = true;
+    h2.innerHTML = "";
+  });
+  scoreO = 0;
+  scoreX = 0;
+  XScore.innerHTML = scoreX;
+  OScore.innerHTML = scoreO;
+});
